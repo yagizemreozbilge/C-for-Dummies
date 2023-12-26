d1fb4c976820ed53af9d213738ef3eddd56f2ffe
Yağız Emre ÖZBİLGE <149567624+yagizemreozbilge@users.noreply.github.com>
2023-12-27 00:58:35 +0300

    Add files via upload

---
+++ Example4/main4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string choice_one = "Good";
+    string choice_two = "Bad";
+
+    if ("Pass the exam") {
+
+        cout << choice_one;
+    }
+    else {
+        cout << choice_two;
+
+    }
+    return 0;
+
+}
