f70cd97e2f1f54a46195263618cfa47b9ab6f6bd
Yağız Emre ÖZBİLGE <149567624+yagizemreozbilge@users.noreply.github.com>
2023-12-27 00:59:32 +0300

    Add files via upload

---
+++ Example4/Example5/main5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int password_1 = 7483910;
+    string password_2 = "Honey I'm home";
+
+    cout << "What is the first password";
+    cin >> password_1;
+    cout << "What is the second password";
+    cin >> password_2;
+
+
+    return 0;
+
+}
