23dc9e62a6b14af24628db21a54a3e02747a5f27
Yağız Emre ÖZBİLGE <149567624+yagizemreozbilge@users.noreply.github.com>
2023-12-27 09:34:39 +0300

    Add files via upload

---
+++ Example7/main7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	char c;
+	int i;
+	float f;
+	double d;
+
+	c = "x";
+	i = 20;
+	f = 7.77;
+	d = 10000.999999999;
+	cout << c;
+	cout << i;
+	cout << f;
+	cout << d;
+
+	return 0;
+}
